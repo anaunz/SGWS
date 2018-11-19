c5129bc20f8a7c9f0fb580fdb1af086405575a12
anaunz <anaunz@gmail.com>
2018-11-20 01:46:14 +0700

    Added About Information

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="about" class="container">
-    <ul class="collection with-header">
-      <li class="collection-header">
-        <router-link to="/" class="fa fa-angle-left fa-lg"></router-link>
-      </li>
-    </ul>
-    <h4>About Us</h4>
-    <p>
-      This website is one of projects in Software Engineering CPE327 class. Smart Garden Watering System is a website that will automatically water your garden.<br>
-      Created By:<br>
-      Thanason Eiamyingsakul 59070501037<br>
-      Thanyaboon Hirundomrong 59070501039<br>
-      Sorawit Therdkiattikoon 59070501070<br>
-      Watcharin Sirinaovakul 59070501093<br>
-    </p>
+  <div id="about" class="container row">
+    <div class="col s8 offset-s2">
+      <h4>About Us</h4>
+      <p>
+        This is a demo version of SGWS implemented to be sent in Software Engineering or CPE327 class.
+      </p>
+      <p>
+        Smart Garden Watering System is a website that will automatically water your garden after the simple settings based on calculation.<br>
+        The project is meant to have quite a lot of features but we only implement 4 modules just like in the document explains.<br>
+      </p>
+      <div class="right-align">
+        <p>
+          Thanason Eiamyingsakul 59070501037<br>
+          Thanyaboon Hirundomrong 59070501039<br>
+          Sorawit Therdkiattikoon 59070501070<br>
+          Watcharin Sirinaovakul 59070501093<br>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
