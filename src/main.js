d2d0fc02badba4d4d01b61831f3712b2e793d1b2
anaunz <anaunz@gmail.com>
2018-11-28 00:51:18 +0700

    Changed to yarn

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 import * as VueGoogleMaps from "vue2-google-maps"
 
@@ -14,10 +12,9 @@ Vue.use(VueGoogleMaps, {
   }
 })
 
-/* eslint-disable no-new */
+Vue.config.productionTip = false
+
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
