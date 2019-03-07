f098d5dc2afb92a618fd6ffd5f307c4f14b8d318
anaunz <anaunz@gmail.com>
2019-03-08 01:31:38 +0700

    Secure Title's info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,8 +4,8 @@
       <div class="container center" style='color:#000000'>
       <i class="fas fa-copyright"></i>
       2018 Smart Garden Watering System |
-      <i class="fas fa-address-book"></i> Contact: torangedevil@gmail.com |
-      <i class="fas fa-phone"></i> Tel: (+66) 87-681-9990
+      <i class="fas fa-address-book"></i> Contact: torxxxxdevil@gmail.com |
+      <i class="fas fa-phone"></i> Tel: (+66) 87-xxx-xxxx
       </div>
     </div>
   </footer>
